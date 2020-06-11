f8a6007e779e1922bc63094fef73f7a0326784ad
Ian J. Watson <ian.james.watson@cern.ch>
2020-06-11 18:06:17 +0900

    pdf weights off

---
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/BFF_ZprimeToTauTau/mass_prod.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+# genproductions should be setup as below
+
+# git clone https://github.com/cms-sw/genproductions.git # doesn't seem to work in slc6 in the chroot
+# cd genproductions
+# git checkout -b mg242legacy origin/mg242legacy
+# cd bin/MadGraph5_aMCatNLO/
+# # Fix up the web address since the model isn't in the usual place for some reason.
+# sed -i -e 's:cms-project-generators.web.cern.ch/cms-project-generators/$model:cms-project-generators.web.cern.ch/cms-project-generators/ZprimeToMuMu/$model:' gridpack_generation.sh
+
+MASS=$1
+
+#cd prod
+mkdir -p BFF_ZprimeToTauTau_M${MASS}_InputCards
+cp cards/production/2017/13TeV/BFF_ZprimeToTauTau/BFF_ZprimeToTauTau_customizecards.dat BFF_ZprimeToTauTau_M${MASS}_InputCards/BFF_ZprimeToTauTau_M${MASS}_customizecards.dat
+cp cards/production/2017/13TeV/BFF_ZprimeToTauTau/BFF_ZprimeToTauTau_extramodels.dat BFF_ZprimeToTauTau_M${MASS}_InputCards/BFF_ZprimeToTauTau_M${MASS}_extramodels.dat
+cp cards/production/2017/13TeV/BFF_ZprimeToTauTau/BFF_ZprimeToTauTau_proc_card.dat BFF_ZprimeToTauTau_M${MASS}_InputCards/BFF_ZprimeToTauTau_M${MASS}_proc_card.dat
+cp cards/production/2017/13TeV/BFF_ZprimeToTauTau/BFF_ZprimeToTauTau_run_card.dat BFF_ZprimeToTauTau_M${MASS}_InputCards/BFF_ZprimeToTauTau_M${MASS}_run_card.dat
+
+sed -i -e "s/set param_card MASS 32 200.0/set param_card MASS 32 ${MASS}.0/" BFF_ZprimeToTauTau_M${MASS}_InputCards/BFF_ZprimeToTauTau_M${MASS}_customizecards.dat
+#sed -i -e "s/set param_card Zprime_Couplings 4 4.000000e-02/set param_card Zprime_Couplings 4 ${DBS}/" BFF_ZprimeToTauTau_M${MASS}_InputCards/BFF_ZprimeToTauTau_M${MASS}_customizecards.dat
+sed -i -e "s/output BFF_ZprimeToTauTau_M200 -nojpeg/output BFF_ZprimeToTauTau_M${MASS} -nojpeg/" BFF_ZprimeToTauTau_M${MASS}_InputCards/BFF_ZprimeToTauTau_M${MASS}_proc_card.dat
+# issue with the cards in the genproduction directory, this should"ve been commented out
+sed -i -e "s/average =  event_norm/!average =  event_norm/" BFF_ZprimeToTauTau_M${MASS}_InputCards/BFF_ZprimeToTauTau_M${MASS}_run_card.dat
+
+sh gridpack_generation.sh BFF_ZprimeToTauTau_M${MASS} BFF_ZprimeToTauTau_M${MASS}_InputCards/ local ALL
+
+mkdir -p prod
+mv BFF_ZprimeToTauTau_M${MASS}_InputCards prod
+mv BFF_ZprimeToTauTau_M${MASS} prod
+mv BFF_ZprimeToTauTau_M${MASS}.log prod/BFF_ZprimeToTauTau_M${MASS}
